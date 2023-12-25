0f65c12f672f2fe89f1af42ced9f8364964c29b1
Huangrunheng <2234044577@qq.com>
2023-12-25 21:05:31 +0800

    Change the repository to ysyx

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-# ICS2023 Programming Assignment
+# "一生一芯"工程项目
 
-This project is the programming assignment of the class ICS(Introduction to Computer System)
-in Department of Computer Science and Technology, Nanjing University.
-
-For the guide of this programming assignment,
-refer to https://nju-projectn.github.io/ics-pa-gitbook/ics2023/
-
-To initialize, run
+这是"一生一芯"的工程项目. 通过运行
 ```bash
 bash init.sh subproject-name
 ```
-See `init.sh` for more details.
+进行初始化, 具体请参考[实验讲义][lecture note].
 
-The following subprojects/components are included. Some of them are not fully implemented.
-* [NEMU](https://github.com/NJU-ProjectN/nemu)
-* [Abstract-Machine](https://github.com/NJU-ProjectN/abstract-machine)
-* [Nanos-lite](https://github.com/NJU-ProjectN/nanos-lite)
-* [Navy-apps](https://github.com/NJU-ProjectN/navy-apps)
+[lecture note]: https://ysyx.oscc.cc/docs/
+
--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 # usage: addenv env_name path
 function addenv() {
   sed -i -e "/^export $1=.*/d" ~/.bashrc
-  echo "export $1=`readlink -e $2`" >> ~/.bashrc
+  echo "export $1=`readlink -f $2`" >> ~/.bashrc
   echo "By default this script will add environment variables into ~/.bashrc."
   echo "After that, please run 'source ~/.bashrc' to let these variables take effect."
   echo "If you use shell other than bash, please add these environment variables manually."
@@ -54,6 +54,20 @@ case $1 in
     ;;
   navy-apps)
     init NJU-ProjectN/navy-apps ics2023 navy-apps true NAVY_HOME
+    ;;
+  nvboard)
+    init NJU-ProjectN/nvboard master nvboard false NVBOARD_HOME
+    ;;
+  npc-chisel)
+    if [ -d npc/playground ]; then
+      echo "chisel repo is already initialized, skipping..."
+    else
+      rm -rf npc
+      init OpenXiangShan/chisel-playground master npc true NPC_HOME
+    fi
+    ;;
+  npc)
+    addenv NPC_HOME npc
     ;;
   *)
     echo "Invalid input..."
