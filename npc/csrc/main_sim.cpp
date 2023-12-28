1b4696931865bb8117838f5961fa1586435e4ba8
Huangrunheng <2234044577@qq.com>
2023-12-28 18:25:21 +0800

    Can output wave

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -1,13 +1,11 @@
 
 // Include common routines
-
+#include <verilated.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 // Include model header, generated from Verilating "top.v"
 #include "Vtop.h"
-#include "verilated_vcd_c.h"
-#include <verilated.h>
 
 int main(int argc, char **argv)
 {
@@ -17,50 +15,34 @@ int main(int argc, char **argv)
     // real project, it is better to start with a more complete example,
     // e.g. examples/c_tracing.
 
-    Verilated::mkdir("logs");
-
     // Construct a VerilatedContext to hold simulation time, etc.
-    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+    VerilatedContext *contextp = new VerilatedContext;
 
     // Pass arguments so Verilated code can see them, e.g. $value$plusargs
     // This needs to be called before you create any model
     contextp->commandArgs(argc, argv);
-    contextp->traceEverOn(true);
-    contextp->randReset(2);
-    contextp->debug(0);
 
     // Construct the Verilated model, from Vtop.h generated from Verilating "top.v"
-
-    const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
-    const std::unique_ptr<VerilatedVcdC> tfp{new VerilatedVcdC()};
-    top->trace(tfp.get(), 0);
-    tfp->open("wave.vcd");
+    Vtop *top = new Vtop{contextp};
 
     // Simulate until $finish
-    // !contextp->gotFinish()
-    while (contextp->time() <= 100)
+    while (!contextp->gotFinish())
     {
-
         int a = rand() & 1;
         int b = rand() & 1;
         top->a = a;
         top->b = b;
-
+        // Evaluate model
         top->eval();
-        tfp->dump(contextp->time());
-        contextp->timeInc(1);
-        printf("[%" PRId64 "] | a = %d, b = %d, f = %d\n", contextp->time(), a, b, top->f);
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
         assert(top->f == (a ^ b));
     }
 
     // Final model cleanup
     top->final();
-    tfp->close();
 
-#if VM_COVERAGE
-    Verilated::mkdir("logs");
-    contextp->coveragep()->write("logs/coverage.dat");
-#endif
+    // Destroy model
+    delete top;
 
     // Return good completion status
     return 0;
