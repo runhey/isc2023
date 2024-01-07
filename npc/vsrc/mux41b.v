a30ee3ad5e260284c136426015fe2e18fe7e816f
tracer-ysyx <tracer@ysyx.org>
2024-01-07 16:18:24 +0800

    > SIM RTL mux41b
    Linux DESKTOP-SMHS4KL 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux
     16:18:24 up 12 days, 20:03,  0 users,  load average: 0.03, 0.02, 0.00

---
+++ npc/vsrc/mux41b.v
@@ -0,0 +1,18 @@
+`include "mux_key.v"
+module top(
+    input [1:0] a0,
+    input [1:0] a1,
+    input [1:0] a2,
+    input [1:0] a3,
+    input [1:0] y,
+    output [1:0] f
+);
+
+MuxKeyInternal #(4, 2, 2, 0) i0 (f, y, 2'b00, {
+    2'b00, a0,
+    2'b01, a1,
+    2'b10, a2,
+    2'b11, a3
+  });
+
+endmodule
---
+++ npc/vsrc/mux_key.v
@@ -0,0 +1,60 @@
+module MuxKeyInternal #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1, HAS_DEFAULT = 0) (
+  output reg [DATA_LEN-1:0] out,
+  input [KEY_LEN-1:0] key,
+  input [DATA_LEN-1:0] default_out,
+  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
+);
+
+  localparam PAIR_LEN = KEY_LEN + DATA_LEN;  // 键长度 + 输出长度
+  wire [PAIR_LEN-1:0] pair_list [NR_KEY-1:0];
+  wire [KEY_LEN-1:0] key_list [NR_KEY-1:0];
+  wire [DATA_LEN-1:0] data_list [NR_KEY-1:0];
+
+  // 生成区块
+  // 你可以理解为 每个元素（键）从 lut 中提取相应的键和数据
+  generate
+    for (genvar n = 0; n < NR_KEY; n = n + 1) begin
+      assign pair_list[n] = lut[PAIR_LEN*(n+1)-1 : PAIR_LEN*n];
+      assign data_list[n] = pair_list[n][DATA_LEN-1:0];
+      assign key_list[n]  = pair_list[n][PAIR_LEN-1:DATA_LEN];
+    end
+  endgenerate
+
+  reg [DATA_LEN-1 : 0] lut_out;  // 存储多路选择器的输出数据。
+  reg hit;  // 标记是否有匹配的键
+  integer i;
+  always @(*) begin
+    lut_out = 0;
+    hit = 0;
+    // 对于每一个键， 检查是否与 key_list 中的键匹配
+    for (i = 0; i < NR_KEY; i = i + 1) begin
+      lut_out = lut_out | ({DATA_LEN{key == key_list[i]}} & data_list[i]);  // 这里有冗余检查
+      hit = hit | (key == key_list[i]); // 太喜欢这种判断了，用0来或上
+    end
+    if (!HAS_DEFAULT) out = lut_out;
+    else out = (hit ? lut_out : default_out);
+  end
+
+endmodule
+
+module MuxKey #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
+  output [DATA_LEN-1:0] out,
+  input [KEY_LEN-1:0] key,
+  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
+);
+  MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 0) i0 (out, key, {DATA_LEN{1'b0}}, lut);
+endmodule
+
+module MuxKeyWithDefault #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
+  output [DATA_LEN-1:0] out,
+  input [KEY_LEN-1:0] key,
+  input [DATA_LEN-1:0] default_out,
+  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
+);
+  MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 1) i0 (out, key, default_out, lut);
+endmodule
+
+
+// NR_KEY 键值对的数量，也是总的数量
+// KEY_LEN 键的长度 不是键的数量， 也就是位宽
+// DATA_LEN 数据长度 也就是输出的宽度
