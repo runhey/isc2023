97d057758e58cdeda9b93f6056f329e389f87bce
tracer-ysyx <tracer@ysyx.org>
2024-01-08 16:42:44 +0800

    > SIM RTL alu
    Linux DESKTOP-SMHS4KL 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux
     16:42:44 up 13 days, 20:27,  0 users,  load average: 0.03, 0.03, 0.00

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -14,6 +14,7 @@ module top (
     output overflow
 );
 controller i0 (clk, control_up, control_down, control_led);
+alu i1 (control_led, a, b, result, carry, zero, overflow);
 endmodule
 
 module controller (
@@ -22,10 +23,6 @@ module controller (
     input control_down,
     output reg [2:0] control_led
 );
-reg button_up;
-reg button_down;
-button_press_detector i0 (control_up, button_up);
-button_press_detector i1 (control_down, button_down);
 
 always @(posedge control_up or posedge control_down) begin
     if(control_up && (control_led < 3'b111)) begin 
@@ -49,5 +46,31 @@ module alu (
     output zero,
     output overflow
 );
+adder i0(a, b, result, carry, zero, overflow); 
+
+// always @(control) begin
+//     case (control) 
+//     3'b000:  adder i0(a, b, result, carry, zero, overflow); 
+//     // default: i0(a, b, result, carry, zero, overflow);
+//     endcase
+// end
+
     
 endmodule
+
+// 加法 输入是补码，输出是原码
+module adder (
+    input [3:0] a,
+    input [3:0] b,
+    output [3:0] result,
+    output carry,
+    output zero,
+    output overflow
+);
+assign {carry, result} = a + b;
+assign zero = ~(| result);
+assign  overflow = a[3] == b[3] && a[3] != result[3]; 
+endmodule
+
+
+
