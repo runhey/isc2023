9266942a86845ec7fc47fccd94113775bc4d3377
tracer-ysyx <tracer@ysyx.org>
2024-01-16 11:29:44 +0800

    > compile NEMU
    Linux DESKTOP-SMHS4KL 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux
     11:29:44 up 1 day,  1:14,  0 users,  load average: 0.03, 0.05, 0.00

---
+++ nemu/src/monitor/sdb/cmd.c
@@ -0,0 +1,25 @@
+#include "sdb.h"
+__attribute__((unused)) static int cmd_si(char *args)
+{
+    return 0;
+}
+__attribute__((unused)) static int cmd_info(char *args)
+{
+    return 0;
+}
+__attribute__((unused)) static int cmd_x(char *args)
+{
+    return 0;
+}
+__attribute__((unused)) static int cmd_p(char *args)
+{
+    return 0;
+}
+__attribute__((unused)) static int cmd_w(char *args)
+{
+    return 0;
+}
+__attribute__((unused)) static int cmd_d(char *args)
+{
+    return 0;
+}
