2a9e89e2596cb83d98d9c8c61a73f1466a1fb10c
tracer-ysyx <tracer@ysyx.org>
2024-01-18 15:45:05 +0800

    > compile NEMU
    Linux DESKTOP-SMHS4KL 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux
     15:45:05 up 3 days,  5:30,  0 users,  load average: 0.01, 0.01, 0.00

--- nemu/src/monitor/sdb/cmd.c
+++ nemu/src/monitor/sdb/cmd.c
@@ -80,7 +80,7 @@ void show_memery(int n, word_t start)
 {
 
     paddr_t address = (start & 0xFFFFFFFF) << 32;
-    address = 0x0004;
+    address = 0x0004 + CONFIG_MBASE;
     for (int i = 0; i < n; i++)
     {
         address += i * 4;
